49e9b31ba95abebecc324a97c0070214c812d2dd
NANI <nani7dam@gmail.com>
2023-03-21 13:46:24 +0100

    init js

---
+++ frontend/src/script.js
@@ -0,0 +1,10 @@
+const loginIcon = document.getElementById("login-icon");
+const cartIcon = document.getElementById("cart-icon");
+
+loginIcon.addEventListener("click", () => {
+  console.log("clicked on the user-icon");
+});
+
+cartIcon.addEventListener("click", () => {
+  console.log("clicked on the cart-icon");
+});
