526c60bf0300eb67b38316ac11bee9b41993871f
nanidam <nani.dam@medieinstitutet.se>
2023-03-26 15:27:34 +0200

    functionality cart

--- frontend/src/script.ts
+++ frontend/src/script.ts
@@ -1,18 +1,30 @@
 const loginIcon = document.getElementById("login-icon") as HTMLElement;
 const cartIcon = document.getElementById("cart-icon") as HTMLElement;
+
+//Login variables
 const loginPage = document.querySelector(".login-page") as HTMLDivElement;
 const loginUser = document.querySelector(
   ".login-user-btn"
 ) as HTMLButtonElement;
 
+//Register variables
 const registerUser = document.querySelector(
   ".user-register"
 ) as HTMLLinkElement;
-
 const registerPage = document.querySelector(".register-page") as HTMLDivElement;
 const registerUserBtn = document.querySelector(
   ".register-user-btn"
 ) as HTMLButtonElement;
+
+//Cart variables
+const cart = document.querySelector(".cart") as HTMLDivElement;
+const cartCancelBtn = document.querySelector(
+  ".cart-cancel-btn"
+) as HTMLButtonElement;
+const cartSendBtn = document.querySelector(
+  ".cart-send-btn"
+) as HTMLButtonElement;
+
 const bgPopup = document.querySelector(".bg-popup") as HTMLDivElement;
 
 //Open login for user
@@ -39,4 +51,13 @@ registerUserBtn.addEventListener("click", () => {
 });
 
 //Open cart
-cartIcon.addEventListener("click", () => {});
+cartIcon.addEventListener("click", () => {
+  cart.classList.remove("hidden");
+  bgPopup.classList.remove("hidden");
+});
+
+//Cancel cart
+cartCancelBtn.addEventListener("click", () => {
+  cart.classList.add("hidden");
+  bgPopup.classList.add("hidden");
+});
