5dca705171b3226613ea9b74a4fbd4dd95f3235b
nanidam <nani.dam@medieinstitutet.se>
2023-04-17 16:36:56 +0200

    edit get all products

--- backend/routes/products.ts
+++ backend/routes/products.ts
@@ -6,7 +6,7 @@ import type { IProduct } from "../src/models/interfaces.js";
 const router = express.Router();
 const api = new API();
 
-// Get products based on specific category
+// Get products of specific category
 router.get("/:id", (req, res) => {
   const id = req.params.id;
   console.log(id);
@@ -24,36 +24,21 @@ router.get("/:id", (req, res) => {
     });
 });
 
-// Get all products or get specific product with id
-// router.get("/", (req, res) => {
-//   if (req.body.id) {
-//     req.app.locals["db"]
-//       .collection("products")
-//       .find({ _id: new ObjectId(req.body.id) })
-//       .project()
-//       .toArray()
-//       .then((results: IProduct) => {
-//         if (results) {
-//           res.status(200).json(results);
-//         } else {
-//           res.status(404).json("Product not found");
-//         }
-//       });
-//   } else {
-//     req.app.locals["db"]
-//       .collection("products")
-//       .find()
-//       .project()
-//       .toArray()
-//       .then((results: IProduct[]) => {
-//         if (results.length > 0) {
-//           res.status(200).json(results);
-//         } else {
-//           res.status(400).json("Products not found");
-//         }
-//       });
-//   }
-// });
+// Get all products
+router.get("/", (req, res) => {
+  req.app.locals["db"]
+    .collection("products")
+    .find()
+    .project()
+    .toArray()
+    .then((results: IProduct[]) => {
+      if (results.length > 0) {
+        res.status(200).json(results);
+      } else {
+        res.status(400).json("Products not found");
+      }
+    });
+});
 
 // Create product
 router.post("/add", function (req: Request, res: Response) {
