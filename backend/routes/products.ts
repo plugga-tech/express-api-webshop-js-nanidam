53373dac103d7f57ee6013a203ce8176d57ed643
nanidam <nani.dam@medieinstitutet.se>
2023-04-17 16:34:36 +0200

    edit router /:id

--- backend/routes/products.ts
+++ backend/routes/products.ts
@@ -1,41 +1,59 @@
 import express, { Request, Response } from "express";
 import { API } from "../src/API.js";
-import { ObjectId } from "mongodb";
+// import { ObjectId } from "mongodb";
 import type { IProduct } from "../src/models/interfaces.js";
 
 const router = express.Router();
 const api = new API();
 
-// Get all products or get specific product with id
-router.get("/", (req, res) => {
-  if (req.body.id) {
-    req.app.locals["db"]
-      .collection("products")
-      .find({ _id: new ObjectId(req.body.id) })
-      .project()
-      .toArray()
-      .then((results: IProduct) => {
-        if (results) {
-          res.status(200).json(results);
-        } else {
-          res.status(404).json("Product not found");
-        }
-      });
-  } else {
-    req.app.locals["db"]
-      .collection("products")
-      .find()
-      .project()
-      .toArray()
-      .then((results: IProduct[]) => {
-        if (results.length > 0) {
-          res.status(200).json(results);
-        } else {
-          res.status(400).json("Products not found");
-        }
-      });
-  }
+// Get products based on specific category
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  req.app.locals["db"]
+    .collection("products")
+    .find({ category: id })
+    .project()
+    .toArray()
+    .then((results: IProduct) => {
+      if (results) {
+        res.status(200).json(results);
+      } else {
+        res.status(404).json("Product not found");
+      }
+    });
 });
+
+// Get all products or get specific product with id
+// router.get("/", (req, res) => {
+//   if (req.body.id) {
+//     req.app.locals["db"]
+//       .collection("products")
+//       .find({ _id: new ObjectId(req.body.id) })
+//       .project()
+//       .toArray()
+//       .then((results: IProduct) => {
+//         if (results) {
+//           res.status(200).json(results);
+//         } else {
+//           res.status(404).json("Product not found");
+//         }
+//       });
+//   } else {
+//     req.app.locals["db"]
+//       .collection("products")
+//       .find()
+//       .project()
+//       .toArray()
+//       .then((results: IProduct[]) => {
+//         if (results.length > 0) {
+//           res.status(200).json(results);
+//         } else {
+//           res.status(400).json("Products not found");
+//         }
+//       });
+//   }
+// });
 
 // Create product
 router.post("/add", function (req: Request, res: Response) {
